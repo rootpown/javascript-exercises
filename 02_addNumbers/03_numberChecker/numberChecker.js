cf11cf0f892fbc9299c942a6e73b11b1355d6907
rootpown <rootpown@gmail.com>
2025-07-13 12:16:17 +0400

    lob

---
+++ 02_addNumbers/03_numberChecker/numberChecker.js
@@ -0,0 +1,9 @@
+function numberChecker(number) {
+  // if (number >= 10) return true;
+  // else if (number >= 9 ) return false;
+  // else return false;
+   return  (number >= 10) ? true : (number >= 9) ? false : false;
+}
+
+// Do not edit below this line
+module.exports = numberChecker;
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let result = '';
+    for( let i = str.length - 1; i>=0;i--){
+        result += str[i]
+    }
+    return result
 };
-
+console.log(reverseString('hello as'));
 // Do not edit below this line
 module.exports = reverseString;
