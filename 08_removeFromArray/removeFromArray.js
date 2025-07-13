d0018286d99649eb992a7b3ee3dfbcf35e48983f
rootpown <rootpown@gmail.com>
2025-07-13 20:22:13 +0400

    08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    // const Arrays = []                   // 2,3,5, => ... 
+    // arr.forEach(elems => { 
+    //     if(!args.includes(elems)) { 
+    //         Arrays.push(elems)
+    //     }
+    // })
+    // return Arrays
+    return arr.filter(elems => !(args.includes(elems)))
 };
-
+removeFromArray([1,2,3,4], 3)
 // Do not edit below this line
 module.exports = removeFromArray;
