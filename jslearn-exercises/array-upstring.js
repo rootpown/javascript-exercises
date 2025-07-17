b3b5a2e6b8b3128fa8b9dceddd2316f2f25792bc
rootpown <rootpown@gmail.com>
2025-07-17 19:36:25 +0400

    camelCase

---
+++ jslearn-exercises/array-upstring.js
@@ -0,0 +1,23 @@
+// Напишите функцию camelize(str), которая преобразует слова, разделенные дефисом, например «my-short-string», в «myShortString» в стиле «camel».
+
+// То есть: удаляет все тире, каждое слово после тире становится заглавным.
+
+
+
+function camelize(str) { 
+  return str.split('-').map((items, index) => {
+      if (index === 0) { 
+          return items.charAt(0).toLowerCase() + items.slice(1);     
+      } else { 
+          return items.charAt(0).toUpperCase() + items.slice(1)
+      }
+  }).join('');
+}
+//
+console.log(camelize("background-color"));
+console.log(camelize("list-style-image") );
+console.log(camelize("-webkit-transition"));
+
+// camelize("background-color") == 'backgroundColor';
+// camelize("list-style-image") == 'listStyleImage';
+// camelize("-webkit-transition") == 'WebkitTransition';
